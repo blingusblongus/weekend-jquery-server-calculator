b8cda7ee7f71909a4bc283b6b58c918fcc5ea3fe
Nick Allen <rutherford.t.b@gmail.com>
2021-10-09 09:23:29 -0500

    added comments, removed try/catch from /calculate POST

--- server/server.js
+++ server/server.js
@@ -1,36 +1,36 @@
+//import packages
 const express = require('express');
 const bodyParser = require('body-parser');
 
+//set configure server
 const app = express();
 const PORT = 5000;
-
 app.use(bodyParser.urlencoded({extended:true}));
 app.use(express.static('server/public'));
 
+//import calculator and history
 const calculator = require('./modules/calculator.js');
 let data = require('./modules/data.js');
 
+//start server
 app.listen(PORT, () => {
     console.log('listening on port', PORT);
 });
 
+// return data
 app.get('/calculate', (req, res) => {
     console.log('/calculate GET')
     res.send(data);
 });
 
+// update data by calling calculator
 app.post('/calculate', (req, res) => {
-    try {
-        console.log('/calculate POST');      
-        data.push({
-            firstTerm: req.body.firstTerm,
-            secondTerm: req.body.secondTerm,
-            op: req.body.op,
-            result: calculator(req.body)
-        })
-        res.sendStatus(201);
-    }catch (error){
-        console.log('server-side /calculate error:', error);
-        res.sendStatus(400);
-    }
+    console.log('/calculate POST');      
+    data.push({
+        firstTerm: req.body.firstTerm,
+        secondTerm: req.body.secondTerm,
+        op: req.body.op,
+        result: calculator(req.body)
+    })
+    res.sendStatus(201);
 })
