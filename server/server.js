7ac3a5633ab71e511c976639c82b882519230470
Nick Allen <rutherford.t.b@gmail.com>
2021-10-12 11:36:38 -0500

    add environment var PORT

--- server/server.js
+++ server/server.js
@@ -4,7 +4,7 @@ const bodyParser = require('body-parser');
 
 //set configure server
 const app = express();
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 app.use(bodyParser.urlencoded({extended:true}));
 app.use(express.static('server/public'));
 
